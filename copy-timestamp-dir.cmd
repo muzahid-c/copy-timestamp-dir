64ec236b97a10c35772c5951be259b7dfa5c2d56
muzahid-c <muzahid@protonmail.com>
2018-03-14 08:49:59 +0600

    Added separate code for different date format and changed a variable name for making it clear

--- copy-timestamp-dir.cmd
+++ copy-timestamp-dir.cmd
@@ -2,8 +2,11 @@
 REM Setting date format legal for file name in windows (dd/mmm/yyyy) 
 SET LOGFILE_DATE=%DATE:~-4,4%-%DATE:~-8,3%-%DATE:~-11,2%
 
+REM Use below code if date format is (m/dd/yyy), Also use it in creating new folder
+REM LOGFILE_DATE=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
+
 REM Added a _ for separating date from time in folder name
-SET FILE_DATE=%DATE:~-4,4%-%DATE:~-8,3%-%DATE:~-11,2%_
+SET FOLDER_DATE=%DATE:~-4,4%-%DATE:~-8,3%-%DATE:~-11,2%_
 
 REM For avoiding space in time
 SET hour=%time:~0,2%
@@ -15,10 +18,9 @@ IF "%secs:~0,1%" == " " SET secs=0%secs:~1,1%
 
 SET LOGFILE_TIME=%hour%-%min%-%secs%
 
-SET FILE_TIME=%hour%%min%%secs%
+SET FOLDER_TIME=%hour%%min%%secs%
 
 SET LOG_FILE=Backup_%LOGFILE_DATE%_%LOGFILE_TIME%.log
-
 
 REM If file is not shared then then use NET USE with user and pass to map the windows administrative share
 SET SRC="\\source\Data"
@@ -26,7 +28,7 @@ SET SRC="\\source\Data"
 REM Keep destination name without space, DON'T put "" in ROOT var value
 SET ROOT=\\Destination_Location
 CD /D %ROOT%
-MKDIR %FILE_DATE%%FILE_TIME% 
+MKDIR %FOLDER_DATE%%FOLDER_TIME% 
 
 SET DEST=%ROOT%%FILE_DATE%%FILE_TIME%
 
